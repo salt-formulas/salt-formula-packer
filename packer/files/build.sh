356934749f424a7af2ea1671318c8bb75a75eb0b
Ales Komarek <mail@newt.cz>
2015-05-07 16:42:11 +0200

    fix move built box

--- packer/files/build.sh
+++ packer/files/build.sh
@@ -8,10 +8,12 @@
 
 cd /srv/packer/templates/{{ system_name }}
 
-RELEASE=$(date +"%Y%m%d_1")
+RELEASE=$(date +"%Y%m%d_01")
 
-packer build -only={{ builder }}-iso -var 'iso_path=/srv/packer/iso' -var 'cm={{ template.provisioner }}' -var 'version=$RELEASE' {{ template.file }}
+packer build -only={{ builder }}-iso -var 'iso_path=/srv/packer/iso' -var 'cm={{ template.provisioner }}' -var 'version=current' {{ template.file }}
 
-mv ./box/{{ builder }}/{{ template.file.split('.')[0] }}-{{ provisioner }}-$RELEASE.box /srv/packer/build/{{ builder }}
+mv ./box/{{ builder }}/{{ template.file.split('.')[0] }}-{{ provisioner }}-current.box /srv/packer/build/{{ builder }}/{{ template.file.split('.')[0] }}-{{ provisioner }}-$RELEASE.box
+
+ln -s /srv/packer/build/{{ builder }}/{{ template.file.split('.')[0] }}-{{ provisioner }}-$RELEASE.box /srv/packer/build/{{ builder }}/{{ template.file.split('.')[0] }}-{{ provisioner }}-current.box
 
 cd /srv/packer/build/scripts
